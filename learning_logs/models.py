19e978aae2fd5e25851f7a64c41225fe30cc4605
Oukey <oukey311@gmail.com>
2019-08-10 19:42:42 +0400

    Добавлена модель Topic, создан superuser и запущена админка

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Topic(models.Model):
     '''Тема, которую изучает пользователь'''
-    text = models.CharField(max_lenght=200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
