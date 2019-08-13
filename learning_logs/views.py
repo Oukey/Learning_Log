45bd1d5ecef8c327acc0d702628846c00db971df
Oukey <oukey311@gmail.com>
2019-08-13 23:03:36 +0400

    Добавлена страница отдельной темы 'topic'

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,3 +10,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')  # запрос к БД на получение объекта Topic
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    '''Выводит одну тему и все её записи'''
+    topic = Topic.objects.get(id=topic_id)  # Запрос к БД
+    entries = topic.entry_set.order_by('-date_added')  # Запрос к БД, '-date_added' сортирует id в обратом порядке
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
